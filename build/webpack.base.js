011b581beff475675a030caa8d5e5725cb1a699f
qinshijie <qinshijie@qq.com>
2024-03-24 14:59:46 +0800

    feat: 配置cross-env环境变量

--- build/webpack.base.js
+++ build/webpack.base.js
@@ -1,7 +1,10 @@
 // webpack.base.js
 const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
+const webpack = require('webpack')
 
+console.log('NODE_ENV', process.env.NODE_ENV)
+console.log('BASE_ENV', process.env.BASE_ENV)
 module.exports = {
   entry: path.join(__dirname, '../src/index.tsx'), // 入口文件
   // 打包文件出口
@@ -37,6 +40,11 @@ module.exports = {
     new HtmlWebpackPlugin({
       template: path.resolve(__dirname, '../public/index.html'), // 模板取定义root节点的模板
       inject: true, // 自动注入静态资源
+    }),
+    // 配置后会把值注入到业务代码里面去,webpack解析代码匹配到process.env.BASE_ENV,就会设置到对应的值
+    // NODE_ENV默认有
+    new webpack.DefinePlugin({
+      'process.env.BASE_ENV': JSON.stringify(process.env.BASE_ENV)
     })
   ]
 }
