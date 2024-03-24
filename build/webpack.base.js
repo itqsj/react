8097d0997e98a3df373dc1c00d41f5dba456e60a
qinshijie <qinshijie@qq.com>
2024-03-24 17:57:42 +0800

    feat: 精确使用loader

--- build/webpack.base.js
+++ build/webpack.base.js
@@ -53,8 +53,28 @@ module.exports = {
       //  test: /.(js|jsx)$/,
       //  use: 'babel-loader'
       // }
+      // test: /.(css|less)$/, //匹配 css和less 文件(css和less可以合并为一起，但是避免使用无用的**loader**解析来提升构建速度因此拆开)
       {
-        test: /.(css|less)$/, //匹配 css和less 文件
+        test: /.\css$/, //匹配 css和less 文件
+        include: [path.resolve(__dirname, '../src')],
+        use: [
+          'style-loader', // 把解析后的**css**代码从**js**中抽离,放到头部的**style**标签中(在运行时做的)
+          'css-loader', // 解析**css**文件代码
+          // {
+          //   loader: 'postcss-loader', // 处理**css**时自动加前缀
+          //   options: {
+          //     postcssOptions: {
+          //       plugins: ['autoprefixer'] // 决定添加哪些浏览器前缀到**css**中
+          //     }
+          //   }
+          // },
+          // **postcss.config.js**是**postcss-loader**的配置文件,会自动读取配置
+          'postcss-loader' // 处理**css**时自动加前缀
+        ]
+      },
+      {
+        test: /.\less$/, //匹配 css和less 文件
+        include: [path.resolve(__dirname, '../src')],
         use: [
           'style-loader', // 把解析后的**css**代码从**js**中抽离,放到头部的**style**标签中(在运行时做的)
           'css-loader', // 解析**css**文件代码
