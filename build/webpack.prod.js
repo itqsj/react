1f85f4523be48488cf77cd632d975c7a17ecec2b
qinshijie <qinshijie@qq.com>
2024-03-24 16:13:44 +0800

    feat: 复制public文件夹

--- build/webpack.prod.js
+++ build/webpack.prod.js
@@ -1,7 +1,24 @@
 // webpack.prod.js
-
+const path = require('path')
 const { merge } = require('webpack-merge')
 const baseConfig = require('./webpack.base.js')
+const CopyPlugin = require('copy-webpack-plugin');
+
 module.exports = merge(baseConfig, {
   mode: 'production', // 生产模式,会开启tree-shaking和压缩代码,以及其他优化
+  plugins: [
+    // 复制文件插件
+    // 开发环境已经在**devServer**中配置了**static**托管了**public**文件夹,在开发环境使用绝对路径可以访问到**public**下的文件,但打包构建时不做处理会访问不到
+    new CopyPlugin({
+      patterns: [
+        {
+          from: path.resolve(__dirname, '../public'), // 复制public下文件
+          to: path.resolve(__dirname, '../dist'), // 复制到dist目录中
+          filter: source => {
+            return !source.includes('index.html') // 忽略index.html
+          }
+        },
+      ],
+    }),
+  ]
 })
