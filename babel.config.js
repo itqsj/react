166f9d7f055f8971aabe3d408484b285dbabdbe9
qinshijie <qinshijie@qq.com>
2024-03-24 16:57:38 +0800

    feat: 配置react模块热更新

--- babel.config.js
+++ babel.config.js
@@ -1,4 +1,6 @@
 // babel.config.js
+const isDEV = process.env.NODE_ENV === 'development' // 是否是开发模式
+
 module.exports = {
   // 执行顺序由右往左,所以先处理ts,再处理jsx,最后再试一下babel转换为低版本语法
   "presets": [
@@ -18,7 +20,8 @@ module.exports = {
     "@babel/preset-typescript" // **ts**语法转换为 **js**
   ],
   "plugins": [
+    isDEV && require.resolve('react-refresh/babel'), // 如果是开发模式,就启动react热更新插件
     // 识别装饰器语法
     ["@babel/plugin-proposal-decorators", { "legacy": true }]
-  ]
+  ].filter(Boolean) // 过滤空值
 }
