be3f7dfcdbabab6aad9c3705f1f160c507585671
qinshijie <qinshijie@qq.com>
2024-03-25 22:16:09 +0800

    feat: 配置打包文件hash

--- build/webpack.base.js
+++ build/webpack.base.js
@@ -12,7 +12,7 @@ module.exports = {
   entry: path.join(__dirname, '../src/index.tsx'), // 入口文件
   // 打包文件出口
   output: {
-    filename: 'static/js/[name].js', // 每个输出js的名称
+    filename: 'static/js/[name].[chunkhash:8].js', // 每个输出js的名称
     path: path.join(__dirname, '../dist'), // 打包结果输出路径
     clean: true, // webpack4需要配置clean-webpack-plugin来删除dist文件,webpack5内置了
     publicPath: '/' // 打包后文件的公共前缀路径
@@ -106,7 +106,7 @@ module.exports = {
           }
         },
         generator:{ 
-          filename:'static/images/[name][ext]', // 文件输出目录和命名
+          filename:'static/images/[name].[contenthash:8][ext]', // 文件输出目录和命名
         },
       },
       // 处理字体和媒体文件
@@ -119,7 +119,7 @@ module.exports = {
           }
         },
         generator:{ 
-          filename:'static/fonts/[name][ext]', // 文件输出目录和命名
+          filename:'static/fonts/[name].[contenthash:8][ext]', // 文件输出目录和命名
         },
       },
       {
@@ -131,7 +131,7 @@ module.exports = {
           }
         },
         generator:{ 
-          filename:'static/media/[name][ext]', // 文件输出目录和命名
+          filename:'static/media/[name].[contenthash:8][ext]', // 文件输出目录和命名
         },
       }
     ]
